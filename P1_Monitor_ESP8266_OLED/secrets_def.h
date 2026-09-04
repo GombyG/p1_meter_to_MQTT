766c48fde66ebf58c35bb8f8d119c681116ddf32
GombyG <gombosi.geza@gmail.com>
2026-09-04 14:37:44 +0200

    default data

---
+++ P1_Monitor_ESP8266_OLED/secrets_def.h
@@ -0,0 +1,9 @@
+// --- MQTT beállítások ---
+// verziószám
+#define SECRET_VERSION             1.00
+// MQTT szerver elérés címe
+#define SECRET_MQTT_SERVER         "192.168.1.50"
+#define SECRET_MQTT_PORT           1883
+#define SECRET_MQTT_USER           "mqtt"
+#define SECRET_MQTT_PASSWORD       "mqtt"
+#define SECRET_GITHUB_URL          "https://raw.githubusercontent.com/Felhasználónév/repositories név/main/firmware_"
